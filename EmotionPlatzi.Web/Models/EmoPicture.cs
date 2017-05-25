40a213c1b42d6b6de5e3891403f42e9d161fe87f
RomaPatch <roman.pons.ridaura@gmail.com>
2017-05-25 12:15:55 +0200

    Add project files.

---
+++ EmotionPlatzi.Web/Models/EmoPicture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Results;
+
+namespace EmotionPlatzi.Web.Models
+{
+    public class EmoPicture
+    {
+        public int Id { get; set; }
+        public string Name {get; set;}
+        [Required]
+        public string Path {get; set;}
+
+        public virtual ObservableCollection<EmoFace> Faces { get; set; }
+
+    }
+}
---
+++ EmotionPlatzi.Web/Util/EmotionHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using EmotionPlatzi.Web.Models;
+using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Emotion = Microsoft.ProjectOxford.Common.Contract.Emotion;
+
+namespace EmotionPlatzi.Web.Util
+{
+    public class EmotionHelper
+    {
+        public EmotionServiceClient emoClient;
+
+        public EmotionHelper(string key)
+        {
+            this.emoClient = new EmotionServiceClient(key);
+        }
+
+        public async Task<EmoPicture> DetectAndExtractFacesAsync(Stream imageSream)
+        {
+           Emotion[] emotions = await  emoClient.RecognizeAsync(imageSream);
+
+            var emoPicture = new EmoPicture();
+            emoPicture.Faces = ExtractFaces(emotions, emoPicture);
+            return emoPicture;
+        }
+
+        private ObservableCollection<EmoFace> ExtractFaces(Emotion[] emotions,EmoPicture emoPicture)
+        {
+            var listFaces = new ObservableCollection<EmoFace>();
+            foreach (var emotion in emotions)
+            {
+                var emoFace = new EmoFace()
+                {
+                    X = emotion.FaceRectangle.Left,
+                    Y = emotion.FaceRectangle.Top,
+                    Width = emotion.FaceRectangle.Width,
+                    Height = emotion.FaceRectangle.Height,
+                    Picture = emoPicture,
+                };
+
+                emoFace.Emotions = ProcessEmotions(emotion.Scores,emoFace);
+                listFaces.Add(emoFace);
+            }
+            return listFaces;
+        }
+
+        private ObservableCollection<EmoEmotion> ProcessEmotions(EmotionScores scores,EmoFace emoFace)
+        {
+           var emotionList = new ObservableCollection<EmoEmotion>();
+
+            var properties = scores.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var filterProperties = properties.Where(p => p.PropertyType == typeof (float));
+
+            var emoType = EmoEmotionEnum.Undetermined;
+
+            foreach (var prop in filterProperties)
+            {
+                if(!Enum.TryParse<EmoEmotionEnum>(prop.Name, out emoType))
+                {
+                    emoType =  EmoEmotionEnum.Undetermined;
+                }
+                var emoEmotion = new EmoEmotion();
+                emoEmotion.Score = (float) prop.GetValue(scores);
+                emoEmotion.EmotionType = emoType;
+                emoEmotion.Face = emoFace;
+                emotionList.Add(emoEmotion);
+            }
+
+            return emotionList;
+        }
+    }
+}
